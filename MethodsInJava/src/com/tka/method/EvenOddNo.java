3e04af9a484ce07077ffd1dbbcc4adf9ce72a4d2
Vishal <vishaltipe555@gmail.com>
2026-03-25 14:02:25 +0530

    Add files via upload

---
+++ MethodsInJava/src/com/tka/method/EvenOddNo.java
@@ -0,0 +1,13 @@
+package com.tka.method;
+
+public class EvenOddNo {
+	public void number( int num) {
+		if(num %2==0) {
+			System.out.println("Number is Even ");
+		}else {
+			System.out.println("Number is Odd  ");
+		}
+	}
+
+
+}
